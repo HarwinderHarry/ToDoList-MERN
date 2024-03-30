d535d6ed747af557d20de78afd21fbe89e907aea
Harwinder kaur <harwinderharry1235@gmail.com>
2024-03-30 03:47:15 -0700

    signup update

--- to-do-listBack/Routes/authRoute.js
+++ to-do-listBack/Routes/authRoute.js
@@ -1,15 +1,19 @@
 import express from 'express';
+import bcrypt from 'bcrypt';
 import userSchema from '../Models/userSchema.js';
 
 //router onject
 const router = express.Router();
 
 //rounting
-//Signin || method post
-export const registerController = async(req,res) =>{
+//Signup || method post
+router.post('/register', async (req,res)=>{
     try {
+        const saltRounds = 10;
         const{name,email,password,phone,gender} =req.body;
-        const user = userSchema({name,email,password,phone,gender});
+        const hashpswd = await bcrypt.hash(password, saltRounds);
+        const user = new userSchema({name, email, password : hashpswd , phone, gender});
+        // console.log(user);
         await user.save().then(()=>{
             res.status(200).json({user:user});
         })
@@ -24,6 +28,41 @@ export const registerController = async(req,res) =>{
             }
         )
     }
- };
+});
+
+
+//Singin
+
+router.post('/login', async (req,res)=>{
+    try {
+    //    const {email, password} = req.body;
+       const user = await userSchema.findOne(req.body);
+       console.log(user);
+
+       if(!email || !password){
+            return res.status(404).send(
+            {
+                success:false,
+                message:'Invaild email or password',
+                error
+            }
+        )}else{
+            return res.status(200).send( req.body);
+        }
+
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).send(
+            {
+                success:false,
+                message:'Error in registeration',
+                error
+            }
+        )
+    }
+
+
+});
 
 export default router;
--- to-do-listBack/index.js
+++ to-do-listBack/index.js
@@ -1,7 +1,10 @@
 import express from "express";
+import cors from "cors";
 import db from "./ConnectDB/db.js";
 import dotenv from 'dotenv';
-import authRoute from "./Routes/authRoute.js";
+// import authRoute from "./Routes/authRoute.js";
+import bcrypt from 'bcrypt';
+import userSchema from './Models/userSchema.js';
 
 const app = express();
 dotenv.config();
@@ -10,13 +13,74 @@ db();
 
 //ConnectRoute
 app.use(express.json());
-app.use("/", authRoute);
+app.use(cors());
+// app.use("/", authRoute);
 
 
 app.get('/',(req,res)=>{
     res.send("<h1>Welcome to backend page todolist</h1>");
 });
 
+
+//API's
+
+//SIGNUP API
+app.post('/register', async (req,res)=>{
+    try {
+        const saltRounds = 10;
+        const{name,email,password,phone,gender} =req.body;
+        const hashpswd = await bcrypt.hash(password, saltRounds);
+        const user = new userSchema({name, email, password : hashpswd , phone, gender});
+        // console.log(user);
+        await user.save().then(()=>{
+            res.status(200).json({user:user});
+        })
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).send(
+            {
+                success:false,
+                message:'Error in registeration',
+                error
+            }
+        )
+    }
+});
+
+//Singin
+
+app.post('/login',async (req,res)=>{
+    try {
+        const{email,password} =req.body;
+        const user = await userSchema.findOne({email:email, password:password});
+        console.log(user);
+        if(!email || !password){
+            return res.status(404).send(
+            {
+                success:false,
+                message:'Invaild email or password',
+                error
+            }
+        )}else{
+            return res.status(200).send( req.body);
+        }
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).send(
+            {
+                success:false,
+                message:'Error in login',
+                error
+            }
+        )
+        
+    }
+//    const user = await userSchema.findOne(req.body);
+//    console.log(user);
+//    res.send(user);
+})
 
 const port = process.env.Port || 8000;
 app.listen(port, () =>{
