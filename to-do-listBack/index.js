bef592acb35a6450788f3b955090054577aa9961
HarwinderHarry <162447030+HarwinderHarry@users.noreply.github.com>
2024-04-01 13:04:44 +0530

    login logic is done

--- to-do-listBack/index.js
+++ to-do-listBack/index.js
@@ -1,10 +1,9 @@
 import express from "express";
 import cors from "cors";
 import db from "./ConnectDB/db.js";
-import dotenv from 'dotenv';
-// import authRoute from "./Routes/authRoute.js";
-import bcrypt from 'bcrypt';
-import userSchema from './Models/userSchema.js';
+import dotenv from "dotenv";
+import bcrypt from "bcrypt";
+import userSchema from "./Models/userSchema.js";
 
 const app = express();
 dotenv.config();
@@ -16,72 +15,71 @@ app.use(express.json());
 app.use(cors());
 // app.use("/", authRoute);
 
-
-app.get('/',(req,res)=>{
-    res.send("<h1>Welcome to backend page todolist</h1>");
+app.get("/", (req, res) => {
+  res.send("<h1>Welcome to backend page todolist</h1>");
 });
-
 
 //API's
 
 //SIGNUP API
-app.post('/register', async (req,res)=>{
-    try {
-        const saltRounds = 10;
-        const{name,email,password,phone,gender} =req.body;
-        const hashpswd = await bcrypt.hash(password, saltRounds);
-        const user = new userSchema({name, email, password : hashpswd , phone, gender});
-        // console.log(user);
-        await user.save().then(()=>{
-            res.status(200).json({user:user});
-        })
-        
-    } catch (error) {
-        console.log(error);
-        res.status(500).send(
-            {
-                success:false,
-                message:'Error in registeration',
-                error
-            }
-        )
-    }
+app.post("/register", async (req, res) => {
+  try {
+    const saltRounds = 10;
+    const { name, email, password, phone, gender } = req.body;
+    const hashpswd = await bcrypt.hash(password, saltRounds);
+
+    let data = userSchema();
+
+    data.name = name;
+    data.email = email;
+    data.password = hashpswd;
+    data.phone = phone;
+    data.gender = gender;
+
+    let result = await data.save();
+
+    res.status(200).json({ result: result });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "Error in registeration",
+      error,
+    });
+  }
 });
 
 //Singin
 
-app.post('/login', async (req,res)=>{
-    try {
-        const{email,password} =req.body;
-        if(!email || !password){
-            return res.status(400).send("Please Add the correct details");
-        }
-        const userMatch = await userSchema.findOne({email:email});
-        // console.log(userMatch);
-        if(userMatch){
-            const passMatch = await bcrypt.compare(password,userMatch.password);
-            if(!passMatch){
-                return res.status(400).send("Invaild Credentials");
-            }else{
-                return res.status(200).send("Login Successfully");
-            }
-        }else{
-            return res.status(400).send("Invaild Credentials 2");
-        }
-        
-    } catch (error) {
-        console.log(error);
-        res.status(500).send(
-            {
-                success:false,
-                message:'Error in login',
-                error
-            }
-        )
+app.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.status(400).send("Please Add the correct details");
     }
+    const userMatch = await userSchema.findOne({ email: email });
+    // console.log(userMatch);
+    if (userMatch) {
+      const passMatch = await bcrypt.compare(password, userMatch.password);
+      if (!passMatch) {
+        return res.status(400).send("Incorrect email or password");
+      } else {
+        return res.status(200).send("Login Successfully");
+      }
+    } else {
+      return res.status(400).send("Invaild Credentials");
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "Error in login",
+      error,
+    });
+  }
 });
 
 const port = process.env.Port || 8000;
-app.listen(port, () =>{
-    console.log("Server Running");
-})
+app.listen(port, () => {
+  console.log("Server Running");
+});
