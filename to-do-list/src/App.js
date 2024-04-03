99b010a0649eaa800c54b89d01f55de5c57e2b26
HarwinderHarry <162447030+HarwinderHarry@users.noreply.github.com>
2024-04-03 17:43:39 +0530

    Dashboard

--- to-do-list/src/App.js
+++ to-do-list/src/App.js
@@ -1,51 +1,15 @@
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'; 
-import LogIn from './Forms/LogIn';
-import SignUp from './Forms/SignUp';
-import Home from './Components/HomePage/Home';
+import {BrowserRouter} from 'react-router-dom'; 
+import Dashboard from './Components/DashboardPage/Dashboard';
 
 
 function App() {
   return (
-    // <div className="App">
-    //   <LogIn />
-    //   <SignUp />
-    // </div>
-    <Router>
-    <div className="App">
-      {/* <nav className="navbar navbar-expand-lg navbar-light fixed-top">
-        <div className="container">
-          <Link className="navbar-brand" to={'/'}>
-            TODO List
-          </Link>
-          <div className="collapse navbar-collapse d-flex justify-content-end" id="navbarTogglerDemo02">
-            <ul className="navbar-nav ml-auto">
-              <li className="nav-item">
-                <Link className="nav-link" to={'/log-in'}>
-                  Login
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to={'/sign-up'}>
-                  Sign up
-                </Link>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav> */}
-      <div className="auth-wrapper">
-        <div className="auth-inner">
-          <Routes>
-            <Route exact path="/" element={<Home /> } />
-            <Route path="/log-in" element={<LogIn />} />
-            <Route path="/sign-up" element={<SignUp />} />
-          </Routes>
-        </div>
-      </div>
+    <div>
+      <BrowserRouter>
+      <Dashboard/>
+      </BrowserRouter>
     </div>
-  </Router>
   );
 }
 
